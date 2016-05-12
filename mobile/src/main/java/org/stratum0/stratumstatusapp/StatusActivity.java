7d25656bfeff21f902cb3f247bd41f474c0592f0
Matthias Uschok <matthias.uschok@ucware.de>
2016-05-12 23:30:35 +0200

    Added generic settings activity

--- mobile/src/main/java/org/stratum0/stratumstatusapp/StatusActivity.java
+++ mobile/src/main/java/org/stratum0/stratumstatusapp/StatusActivity.java
@@ -1,9 +1,11 @@
 package org.stratum0.stratumstatusapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class StatusActivity extends AppCompatActivity {
 
@@ -18,5 +20,18 @@ public class StatusActivity extends AppCompatActivity {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.menu_status, menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.settings: {
+                Intent settingsIntent = new Intent(this, SettingsActivity.class);
+                startActivity(settingsIntent);
+                return true;
+            }
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
